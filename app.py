ab089a41bdccfa0771c31b5394a55c49bc34bd22
hridesh16 <hrideshroshan367@gmail.com>
2024-11-13 17:02:35 +0000

    index added

--- app.py
+++ app.py
@@ -45,6 +45,9 @@ def add():
     return render_template('add.html')
 
   return '{"Result":"Success"}'
+@app.route("/index.html") #Default - Show Data
+def index():
+  return render_template('index.html')
 @app.route("/") #Default - Show Data
 def hello(): # Name of the method
   cur = mysql.cursor() #create a connection to the SQL instance
