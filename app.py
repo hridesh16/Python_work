db3f464631332e5ab60cbc75c0f6edd9874b40c7
hridesh16 <hrideshroshan367@gmail.com>
2024-11-13 17:05:14 +0000

    added

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def add():
     return render_template('add.html')
 
   return '{"Result":"Success"}'
-@app.route("/") #Default - Show Data
+@app.route("/index.html") #Default - Show Data
 def index():
   return render_template('index.html')
 @app.route("/") #Default - Show Data
